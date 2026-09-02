53b6224b02a7f257601196e3bf6f6d6c5e904c16
Prathyush <prathyush0321@gmail.com>
2026-09-02 11:05:24 +0530

    feat(v3): add Media3 playback controller

---
+++ app/src/main/java/com/prathyushin/musicgallery/playback/PlaybackController.kt
@@ -0,0 +1,32 @@
+package com.prathyushin.musicgallery.playback
+
+import android.content.Context
+import androidx.media3.common.MediaItem
+import androidx.media3.exoplayer.ExoPlayer
+import com.prathyushin.musicgallery.model.Track
+
+class PlaybackController(context: Context) {
+    private val player = ExoPlayer.Builder(context).build()
+
+    fun play(track: Track) {
+        val item = MediaItem.Builder()
+            .setMediaId(track.id.toString())
+            .setUri(track.artworkUri ?: return)
+            .setMediaMetadata(
+                androidx.media3.common.MediaMetadata.Builder()
+                    .setTitle(track.title)
+                    .setArtist(track.artist)
+                    .setAlbumTitle(track.album)
+                    .build()
+            )
+            .build()
+        player.setMediaItem(item)
+        player.prepare()
+        player.play()
+    }
+
+    fun pause() = player.pause()
+    fun resume() = player.play()
+    fun release() = player.release()
+    fun isPlaying(): Boolean = player.isPlaying
+}
