e80459fb8e9d3f0a64356938ac7a42d8bc4684c8
Prathyush <prathyush0321@gmail.com>
2026-09-02 11:47:48 +0530

    V3.1: add centralized app version identity

---
+++ app/src/main/java/com/prathyushin/musicgallery/AppVersion.kt
@@ -0,0 +1,7 @@
+package com.prathyushin.musicgallery
+
+object AppVersion {
+    const val NAME = "3.1.0-alpha01"
+    const val VERSION_CODE = 31
+    const val RELEASE_TITLE = "The first genuinely usable Music Gallery release"
+}
