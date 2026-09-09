d0e8d5556d778d623ab9d3a291cdf91365350f48
Prathyush <prathyush0321@gmail.com>
2026-09-09 16:14:19 +0530

    release: bump MusicGallery to 4.3.0 alpha

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.prathyushin.musicgallery"
         minSdk = 28
         targetSdk = 37
-        versionCode = 43
-        versionName = "4.2.1-alpha01"
+        versionCode = 44
+        versionName = "4.3.0-alpha01"
     }
     buildFeatures { compose = true }
 }
